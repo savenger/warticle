9e186b38f6e4ab9c0c8953a25b0a47c09dc00b80
Felan7 <50790924+Felan7@users.noreply.github.com>
2022-01-29 12:42:46 +0100

    Added UI to world

--- Scripts/audio_system.gd
+++ Scripts/audio_system.gd
@@ -6,3 +6,13 @@ func set_music_volume(new_volume):
 
 func _on_audio_system_sfx_volume(new_volume):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), new_volume)
+
+
+func _on_Button_MusicMute_toggled(button_pressed):
+	print("Music: " + str(button_pressed))
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"),!button_pressed)
+
+
+func _on_Button_SFXMute_toggled(button_pressed):
+	print("SFX: " + str(button_pressed))
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"),!button_pressed)
