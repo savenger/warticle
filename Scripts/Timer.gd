06be651b211c2f3129f33079dac25f27b441e464
Felan7 <50790924+Felan7@users.noreply.github.com>
2022-01-29 12:57:24 +0100

    Added UI, Sound and resorted
    Added UI Counter functionality
    Added Sound system Functionality
    Changed some file locations

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var count = 0
+signal count(new_value)
 
 func _process(delta):
 	count += 1.0/60.0
+	emit_signal("count", count)
--- Scripts/particles.gd
+++ Scripts/particles.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const Particle = preload("res://particle.tscn")
+const Particle = preload("res://Scenes/particle.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
