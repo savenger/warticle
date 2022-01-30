85342df23f847576aeaea2d6eefdc352bed8c4bf
Cirioke <joeldukowski@gmail.com>
2022-01-30 15:53:30 +0100

    fixed bug

--- HighscoreList.gd
+++ HighscoreList.gd
@@ -4,7 +4,7 @@ extends VBoxContainer
 func _ready():
 	for x in Globals.highscores:
 		var new_label = Label.new()	
-		new_label.text = x
+		new_label.text = str(x)
 		add_child(new_label)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
